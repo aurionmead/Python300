1a0ced289b2150e366ab10dbb456a043df1e6c45
aurionmead <78833184+aurionmead@users.noreply.github.com>
2021-03-29 12:22:50 +1300

    Add files via upload

---
+++ 00_AS91906.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import random
+
+class sell:
+    def __innit__(self, parent):
+        print("Hello world")
+
+#main route
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mead Magazine & Comics")
+    something = sell(root)
+    root.mainloop()
